8aa57243a7bec1003b926701bd00f01dbddb425f
cyperdark <aburachenko2000@gmail.com>
2022-11-16 05:24:03 +0300

    more utils

--- index.ts
+++ index.ts
@@ -1,9 +1,48 @@
-import { read_file, write_file } from './utils/index';
+import { read_file, write_file, parse_keypairs } from './utils/index';
+import { ini_type, ini_object } from './types';
 
 
-const main = (file_path: string) => {
+const parse = (file_path: string): ini_type => {
+  const result: ini_type = Object.assign({}, ini_object);
+  const raw_data = read_file(file_path);
 
+  let mania_object: any = {};
+  let is_mania = false;
+
+  // parse allowed properties & sort it
+  const parse_pairs = parse_keypairs(raw_data);
+  for (let i = 0; i < parse_pairs.length; i++) {
+    const d = parse_pairs[i];
+
+    if (d.n == 'Keys') {
+      if (mania_object.Keys) {
+        if (!result.Mania) result.Mania = [];
+        result.Mania.push(mania_object);
+        mania_object = {};
+      };
+
+      is_mania = true;
+    };
+
+    if (is_mania) mania_object[d.n] = d.v;
+    // @ts-ignore
+    else result[d.n] = d.v;
+  };
+
+  const object = Object.keys(result);
+  for (let i = 0; i < object.length; i++) {
+    const key = object[i];
+    // @ts-ignore
+    const value = result[key];
+    if (typeof value == 'object') continue;
+    // @ts-ignore
+    if (value == -1) delete result[key];
+    // @ts-ignore
+    if (value == '') delete result[key];
+  }
+
+  return result;
 };
 
 
-export default main;
+export { parse as Parse };
--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,34 @@
+import { file_keypairs } from './regex';
+import { allowed_properties } from '../types/index';
 import fs from 'fs';
+
 
 const read_file = (file_path: string) => fs.readFileSync(file_path, 'utf-8');
 const write_file = (file_path: string, data: string) => fs.writeFileSync(file_path, data, 'utf-8');
+
+
+export const parse_keypairs = (data: string) => {
+  const result: { n: string, v: string }[] = [];
+
+  let is_mania = false;
+
+  const total = data.split('\n');
+  for (let i = 0; i < total.length; i++) {
+    const d = total[i].trim();
+
+    const parse = file_keypairs.exec(d);
+    if (!parse) continue;
+
+    if (parse.groups.name == 'Keys') is_mania = true;
+
+    if (!d.startsWith(parse.groups.name)) continue;
+    if (!allowed_properties.includes(parse.groups.name.split(/\d+/).join(''))) continue;
+
+    result.push({ n: parse.groups.name, v: parse.groups.id.split('//')[0].trim() });
+  };
+
+  return result;
+};
+
 
 export { read_file, write_file };
