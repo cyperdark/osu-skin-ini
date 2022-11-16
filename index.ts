b7a8f455c61c26cd3044cf60d13ecc62e0104832
cyperdark <aburachenko2000@gmail.com>
2022-11-16 05:58:04 +0300

    write function

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { read_file, write_file, parse_keypairs } from './utils/index';
-import { ini_type, ini_object } from './types';
+import { ini_type, ini_object, ini_category } from './types';
 
 
 const parse = (file_path: string): ini_type => {
@@ -29,9 +29,9 @@ const parse = (file_path: string): ini_type => {
     else result[d.n] = d.v;
   };
 
-  const object = Object.keys(result);
-  for (let i = 0; i < object.length; i++) {
-    const key = object[i];
+  const array = Object.keys(result);
+  for (let i = 0; i < array.length; i++) {
+    const key = array[i];
     // @ts-ignore
     const value = result[key];
     if (typeof value == 'object') continue;
@@ -45,4 +45,71 @@ const parse = (file_path: string): ini_type => {
 };
 
 
-export { parse as Parse };
+const write = (input: ini_type) => {
+  let result = '//Formatted by ck // pepega tools // cyperdark#6890 // https://github.com/cyperdark/ck-tools\n';
+
+  const groups: {
+    General: object[];
+    Colours: object[];
+    Fonts: object[];
+    CatchTheBeat: object[];
+    Mania: object[];
+  } = {
+    General: [],
+    Colours: [],
+    Fonts: [],
+    CatchTheBeat: [],
+    Mania: input.Mania,
+  };
+
+  const array = Object.keys(input);
+  for (let i = 0; i < array.length; i++) {
+    const key = array[i];
+    // @ts-ignore
+    const value = input[key];
+    if (typeof value == 'object') continue;
+
+    const array_category = Object.keys(ini_category);
+    for (let i = 0; i < array_category.length; i++) {
+      const d = array_category[i];
+      // @ts-ignore
+      const dv = ini_category[d];
+
+      const find = dv.includes(key);
+      if (find) {
+        // @ts-ignore
+        groups[d].push({ n: key, v: value, });
+        break;
+      };
+    };
+  };
+
+  const array_groups = Object.keys(groups);
+  for (let i = 0; i < array_groups.length; i++) {
+    const key = array_groups[i];
+    // @ts-ignore
+    const value = groups[key];
+    if (value.length == 0) continue;
+
+    result += `\n\n[${key}]\n`;
+
+    for (let a = 0; a < value.length; a++) {
+      const d = value[a];
+
+      if (!d.n) {
+        const destruct = Object.keys(d);
+        destruct.filter(r => result += `${r == 'Keys' && a != 0 ? '\n\n' : ''}${r}: ${d[r]}\n`);
+
+        continue;
+      };
+
+      result += `${d.n}: ${d.v}\n`;
+      // result += `${d.n}: ${d.v}${i == array_groups.length - 1 && a == value.length - 1 ? '' : '\n'}`;
+    };
+  };
+
+  return result;
+};
+
+
+export { parse as Parse, write as Write };
